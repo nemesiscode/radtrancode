6060e07e97b83b4e161b26e4de4fb21269ba684f
Perf. Patrick Irwin <patrick.irwin@physics.ox.ac.uk>
2012-05-09 16:39:22 +0000

    Added reservegas.f, which reads in the reference gas ID file, including the molecular weights and terrestial abundance ratios.
    git-svn-id: https://scm.physics.ox.ac.uk/svn/radtrancode/trunk@73 4dc0e162-f54d-4b9e-8a18-81c7a7f2ddce

---
+++ radtran/radtran/reservegas.f
@@ -0,0 +1,21 @@
+      SUBROUTINE RESERVEGAS
+C     **************************************************************
+C     Subroutine to read in relative abundances of gases from the
+C     reference gasinfo.dat file, which is expected to reside in the
+C     raddata/ directory.
+C
+C     Pat Irwin		Original	9/5/12
+C
+C     **************************************************************
+      INCLUDE '../includes/dbcom.f'
+
+      GASFIL='gasinfo04.dat'
+      CALL DATARCHIVE(GASFIL)
+      DBLUN=2
+
+      CALL RDGAS
+
+      RETURN
+
+      END
+      

9ddb9a21dbbf1c71fd0dd04806c3961485d2478f
Perf. Patrick Irwin <patrick.irwin@physics.ox.ac.uk>
2012-05-11 12:20:22 +0000

    New subroutines for defining number of model parameters for different profile parameterisation schemes.
    git-svn-id: https://scm.physics.ox.ac.uk/svn/radtrancode/trunk@92 4dc0e162-f54d-4b9e-8a18-81c7a7f2ddce

---
+++ nemesis/logflag.f
@@ -0,0 +1,55 @@
+      integer function logflag(ivar,imod,ip)
+C     **************************************************************
+C     Simple function to define whether a particular variable is held as
+C     a log variable within Nemesis.
+C
+C     Input variable
+C	ivar	integer	Profile ID (varident(1)).
+C	imod	integer	Required parameterisation scheme ID.
+C	ip	integer	Element of parameterisation vector
+C
+C     Output variable
+C	logflag	integer	Set to 1 if log, 0 otherwise.
+C
+C     Pat Irwin	11/5/12
+C
+C     **************************************************************
+      implicit none
+      integer ivar,imod,iflag,ip
+
+      iflag=0
+
+      if(ivar.ne.0)then
+C       Variable is not temperature  - may need to take exponent
+        if(imod.eq.0)iflag=1		 ! continuous profile
+        if(imod.eq.1.and.ip.eq.1)iflag=1 ! knee profile         
+        if(imod.eq.4.and.ip.eq.1)iflag=1 ! variable knee profile
+        if(imod.eq.6.and.ip.eq.1)iflag=1 ! Venus cloud profile
+        if(imod.eq.7.and.ip.eq.1)iflag=1 ! extended profile         
+        if(imod.eq.8.and.ip.eq.1)iflag=1 ! variable knee profile
+        if(imod.eq.9.and.ip.eq.1)iflag=1 ! variable knee profile
+      endif
+
+      if(imod.eq.1.and.ip.eq.2)iflag=1 ! log fsh - fixed knee
+      if(imod.eq.4.and.ip.eq.2)iflag=1 ! log fsh - var. knee
+      if(imod.eq.4.and.ip.eq.3)iflag=1 ! variable knee profile
+      if(imod.eq.6.and.ip.eq.2)iflag=1 ! Venus cloud profile
+      if(imod.eq.7.and.ip.eq.2)iflag=1 ! log fsh - extended
+      if(imod.eq.8.and.ip.eq.2)iflag=1 ! log fsh - var. knee
+      if(imod.eq.8.and.ip.eq.3)iflag=1 ! variable knee profile
+      if(imod.eq.9.and.ip.eq.2)iflag=1 ! log fsh - var. knee
+
+      if(imod.eq.3)iflag=1	! Log scaling factor
+      if(imod.eq.10)iflag=1	! Log scaling factor
+      if(imod.eq.11)iflag=1	! Log scaling factor
+
+      if(ivar.eq.888)iflag=1	! Surface albedo spectrum
+      if(ivar.eq.666)iflag=1	! Tangent pressure
+
+      logflag=iflag
+
+      return
+
+      end
+
+
---
+++ nemesis/npvar.f
@@ -0,0 +1,40 @@
+      integer function npvar(imod,npro)
+C     **************************************************************
+C     Simple function to define the number of elements in the measurement
+C     vector, xn, needed to describe an atmospheric profile, depending on
+C     the require parameterisation scheme.
+C
+C     Input variable
+C	imod	integer	Require parameterisation scheme ID.
+C
+C     Output variable
+C	npvar	integer	Number of elements in xn required.
+C
+C     Pat Irwin	11/5/12
+C
+C     **************************************************************
+      implicit none
+      integer imod,np,npro
+
+      np=1
+      if(imod.le.11)then
+        if(imod.eq.0)np = npro
+        if(imod.eq.1)np = 2
+        if(imod.eq.4)np = 3
+        if(imod.eq.6)np = 2
+        if(imod.eq.7)np = 2
+        if(imod.eq.8)np = 3
+        if(imod.eq.9)np = 3
+        if(imod.eq.10)np = 4
+        if(imod.eq.11)np = 2
+      else
+       print*,'npvar:  Model paramterisation not defined = ',imod
+      endif
+
+      npvar=np
+
+      return
+
+      end
+
+

41aa73fd158c6dc988de03673faa26af3582ccde
Perf. Patrick Irwin <patrick.irwin@physics.ox.ac.uk>
2012-06-06 09:49:21 +0000

    Added IO3 to pass list to include Bass Ozone UV absorption.
    git-svn-id: https://scm.physics.ox.ac.uk/svn/radtrancode/trunk@131 4dc0e162-f54d-4b9e-8a18-81c7a7f2ddce

--- radtran/rtm_util/readflags.f
+++ radtran/rtm_util/readflags.f
@@ -1,4 +1,5 @@
-      subroutine readflags(runname,inormal,iray,ih2o,ich4,iptf)
+      subroutine readflags(runname,inormal,iray,ih2o,ich4,io3,
+     &  iptf)
 C     ********************************************************************
 C     Subroutine to read in the runname.fla file, which contains a number
 C     of spectral processing flags.
@@ -11,13 +12,14 @@ C	inormal	integer	Hydrogen ortho/para ratio. 0=eqm, 1=normal
 C	iray	integer	Rayleigh optical depth off(0) or on(1)
 C	ih2o	integer	Include H2O continuum (1)
 C	ich4	integer	Include Karkoschka methane continuum (1)
+C	io3	integer	Include Bass UV ozone absorption (1)
 C	iptf	integer	Use modified CH4 high-T part. func. (1)
 C
 C     Pat Irwin	21/2/12
 C
 C     ********************************************************************
       character*(*) runname
-      integer inormal,iray,ih2o,ich4,iptf
+      integer inormal,iray,ih2o,ich4,io3,iptf
 
       call file(runname,runname,'fla')
 
@@ -26,6 +28,7 @@ C     ********************************************************************
       read(12,*)iray
       read(12,*)ih2o
       read(12,*)ich4
+      read(12,*)io3
       read(12,*)iptf
       close(12)
 

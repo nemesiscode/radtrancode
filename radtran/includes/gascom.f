c451f51374ee679bf42a341e8014f65ab2de67d9
Perf. Patrick Irwin <patrick.irwin@physics.ox.ac.uk>
2012-06-06 09:51:28 +0000

    Added Ozone UV continuum absorption.
    git-svn-id: https://scm.physics.ox.ac.uk/svn/radtrancode/trunk@134 4dc0e162-f54d-4b9e-8a18-81c7a7f2ddce

--- radtran/includes/gascom.f
+++ radtran/includes/gascom.f
@@ -1,6 +1,6 @@
 C     Define the fixed GASCON parameters
-      INTEGER ICH4,IH2O
+      INTEGER ICH4,IH2O,IO3
 C     ----------------------------------------------------------------------
-      COMMON /GASCONDEF/ICH4,IH2O
+      COMMON /GASCONDEF/ICH4,IH2O,IO3
 C     ----------------------------------------------------------------------
 

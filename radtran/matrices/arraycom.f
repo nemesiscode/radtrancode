00436ce13b4490acdddf1d8b406cce0f017842bc
Perf. Patrick Irwin <patrick.irwin@physics.ox.ac.uk>
2013-03-01 09:58:41 +0000

    File no longer needed.
    git-svn-id: https://scm.physics.ox.ac.uk/svn/radtrancode/trunk@253 4dc0e162-f54d-4b9e-8a18-81c7a7f2ddce

--- radtran/matrices/arraycom.f
+++
@@ -1,6 +0,0 @@
-C     **********************************************************
-C     Define maximum size of inversion routines. 
-      INTEGER IDIM
-      PARAMETER (IDIM=1024)
-C     PGJI	17/2/12
-C     **********************************************************

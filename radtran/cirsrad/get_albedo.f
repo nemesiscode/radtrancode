ff6f2a2925e0e2ec88d29de24da6cd5e636ce35f
Perf. Patrick Irwin <patrick.irwin@physics.ox.ac.uk>
2013-03-20 17:37:28 +0000

    Modified to read albedo from the emissivity file, rather than a separate albedo file.
    git-svn-id: https://scm.physics.ox.ac.uk/svn/radtrancode/trunk@268 4dc0e162-f54d-4b9e-8a18-81c7a7f2ddce

--- radtran/cirsrad/get_albedo.f
+++ radtran/cirsrad/get_albedo.f
@@ -1,23 +1,24 @@
-      double precision function get_albedo(nalb,valb,alb,x)
+      double precision function get_albedo(nem,vem,emissivity,x)
 C     **********************************************************
 C     Routine to interpolate a surface albedo spectrum to the required
 C     wavenumber/wavelength.
 C
-C     Pat Irwin	17/11/05
+C     Pat Irwin	17/11/05 Original
+C     Pat Irwin 20/3/13  Revised to get albedo from emissivity spectrum
 C
 C     **********************************************************
-      integer nalb
-      real valb(nalb),alb(nalb),x,y
+      integer nem
+      real vem(nem),emissivity(nem),x,y
 
-      if(x.le.valb(1))then
-       y = alb(1)
-      else if(x.ge.valb(nalb))then
-       y = alb(nalb)
+      if(x.le.vem(1))then
+       y = emissivity(1)
+      else if(x.ge.vem(nem))then
+       y = emissivity(nem)
       else
-       call verint(valb,alb,nalb,y,x)
+       call verint(vem,emissivity,nem,y,x)
       endif
 
-      get_albedo = dble(y)
+      get_albedo = dble(1.0-y)
 
       return
 

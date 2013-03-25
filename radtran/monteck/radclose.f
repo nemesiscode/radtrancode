0605538cb3cd32abc6dad31a3377bb6374fbca90
Perf. Patrick Irwin <patrick.irwin@physics.ox.ac.uk>
2013-03-25 12:14:53 +0000

    Modified method of calculating closest distance to make it more compatible with approach of hitsphere.f to avoid small rounding differences leading to large and difficult to track errors!
    git-svn-id: https://scm.physics.ox.ac.uk/svn/radtrancode/trunk@288 4dc0e162-f54d-4b9e-8a18-81c7a7f2ddce

--- radtran/monteck/radclose.f
+++ radtran/monteck/radclose.f
@@ -4,11 +4,13 @@ C     Simple routine to find the closest approach to the origin
 C     of a line with initial position PVEC and direction DVEC
 C     ********************************************************
       IMPLICIT NONE
-      REAL PVEC(3),DVEC(3),TVEC(3),SUM1,SUM2,SUM3,SUM4
+      REAL PVEC(3),DVEC(3),TVEC(3),SUM1,SUM2
+      REAL RMIN,LMIN,A,B,C
       INTEGER I
 
 C     Need to see if photon is moving towards or away from the closest approach
 C     to the origin
+
       SUM1=0.
       SUM2=0.
       DO I=1,3
@@ -24,23 +26,21 @@ C      Photon is moving upwards and so cannot hit the surface
       ELSE
 C      Photon is moving downwards and so could potentially strike the surface
 C      Find closest approach
-       TVEC(1)=PVEC(2)*DVEC(3)-PVEC(3)*DVEC(2)
-       TVEC(2)=PVEC(3)*DVEC(1)-PVEC(1)*DVEC(3)
-       TVEC(3)=PVEC(1)*DVEC(2)-PVEC(2)*DVEC(1)
 
-       SUM3=0.
-       SUM4=0.
-       DO I=1,3
-         SUM3=SUM3+TVEC(I)**2
-         SUM4=SUM4+DVEC(I)**2
-       ENDDO
+C      R=PVEC+L*DVEC. Distance from origin = R^2. Differentiate
+C      with respect to L and find value of L when distance is minumum.
+C      Then calculate the minimum distance. 
+       A=DVEC(1)**2 + DVEC(2)**2 + DVEC(3)**2
+       B=2.0*(PVEC(1)*DVEC(1)+PVEC(2)*DVEC(2)+PVEC(3)*DVEC(3))
+       C=PVEC(1)**2+PVEC(2)**2+PVEC(3)**2
 
-       RADCLOSE=SQRT(SUM3/SUM4)
+       LMIN = -B/(2.*A)
+       RMIN = A*LMIN**2 + B*LMIN+C
+
+        RADCLOSE=SQRT(RMIN)
 
       ENDIF
  
-
-
       RETURN
 
       END

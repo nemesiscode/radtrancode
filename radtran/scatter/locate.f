24da32dd46e050e6a05824bb317dd9e2f9823a5b
Perf. Patrick Irwin <patrick.irwin@physics.ox.ac.uk>
2013-04-25 16:10:43 +0000

    Updated to deal better with inputs that match the values at the start or the end of the array.
    git-svn-id: https://scm.physics.ox.ac.uk/svn/radtrancode/trunk@308 4dc0e162-f54d-4b9e-8a18-81c7a7f2ddce

--- radtran/scatter/locate.f
+++ radtran/scatter/locate.f
@@ -1,17 +1,28 @@
       SUBROUTINE LOCATE(XX,N,X,J)
       DIMENSION XX(N)
+C     Given an array xx(1:n), and given a value x, returns a value
+C     j such that x is between xx(j) and xx(j+1). xx(1:n) must be monotonic, 
+C     either increasing or decreasing. j=0 or j=n is returned to indicate that
+C     x is out of range.
     
       JL=0
       JU=N+1
 10    IF(JU-JL.GT.1)THEN
         JM=(JU+JL)/2
-        IF((XX(N).GT.XX(1)).EQV.(X.GT.XX(JM)))THEN
+        IF((XX(N).GE.XX(1)).EQV.(X.GE.XX(JM)))THEN
           JL=JM
         ELSE
           JU=JM
         ENDIF
       GO TO 10
       ENDIF
-      J=JL
+      IF(X.EQ.XX(1))THEN
+       J=1
+      ELSE IF(X.EQ.XX(N))THEN
+       J=N-1
+      ELSE
+       J=JL
+      ENDIF
+
       RETURN
       END

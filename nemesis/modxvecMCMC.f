0216b42c8a0aca68549fd684a6fcf163989369a3
Perf. Patrick Irwin <patrick.irwin@physics.ox.ac.uk>
2013-06-12 10:11:23 +0000

    Bug fixes.
    git-svn-id: https://scm.physics.ox.ac.uk/svn/radtrancode/trunk@327 4dc0e162-f54d-4b9e-8a18-81c7a7f2ddce

--- nemesis/modxvecMCMC.f
+++ nemesis/modxvecMCMC.f
@@ -43,11 +43,20 @@ C     ****************************************************************
 
       real x0(mx),xn(mx),sx(mx,mx),pref(maxpro),eref(maxpro)
       real delp,xfac,pknee,edeep,xdeep,err(mx),xtry(mx)
-      real efsh,xfsh,varparam(mvar,mparam),gasdev
-      real ref(maxpro),clen,pi,dx,xl,xphi,xamp
+      real efsh,xfsh,varparam(mvar,mparam),gasdev1
+      real ref(maxpro),clen,pi,dx,xl,xphi,xamp,sum,f
       parameter(pi=3.1415927)
       integer varident(mvar,3),ivar,nvar,nlevel
       character*100 opfile,buffer,ipfile
+
+
+C      print*,idum,npro,nvar
+C      do i=1,nvar
+C       print*,(varident(i,j),j=1,3)
+C       print*,(varparam(i,j),j=1,5)
+C      enddo
+C      print*,nx
+C      print*,(x0(i),i=1,nx)
 
       do i=1,nx
         err(i)=sqrt(sx(i,i))
@@ -59,72 +68,80 @@ C     ****************************************************************
 C         ********* continuous profile ************************
 
 C           Randomly modify the trial profile
+C            print*,ivar,npro
+            np=npro
             do i=1,np
-             xtry(i)=err(ix+i)*gasdev(idum)
+             xtry(i)=err(ix+i)*gasdev1(idum)
+C             print*,i,x0(ix+i),err(ix+i),xtry(i)
             enddo
 
 C           Now smooth with covariance matrix
             do i=1,np
-             xn(ix+i)=0
+             xn(ix+i)=0.
+             sum=0.
              do j=1,np
-        xn(ix+i)=xn(ix+i) + (sx(ix+i,ix+j)/sx(ix+i,ix+i))*xtry(j)**2
+               f=sqrt(sx(ix+i,ix+j)/sx(ix+i,ix+i))
+C              print*,j,f
+               xn(ix+i)=xn(ix+i) + f*xtry(j)
+              sum=sum+f
              enddo
-             xn(ix+i)=xn(ix+i)*sqrt(xn(ix+i))
+             xn(ix+i)=x0(ix+i)+xn(ix+i)/sum
             enddo          
 
+      
           elseif(varident(ivar,3).eq.1)then
 C         ******** profile held as deep amount, fsh and knee pressure **
             np = 2
             do i=1,np
-             xn(ix+i)=x0(ix+i)+err(ix+i)*gasdev(idum)
+             xn(ix+i)=x0(ix+i)+err(ix+i)*gasdev1(idum)
             enddo
           elseif(varident(ivar,3).eq.2)then  
 C         **** Simple scaling factor of reference profile *******
             np = 1
             do i=1,np
-             xn(ix+i)=x0(ix+i)+err(ix+i)*gasdev(idum)
+             xn(ix+i)=x0(ix+i)+err(ix+i)*gasdev1(idum)
             enddo
           elseif(varident(ivar,3).eq.3)then  
 C         **** Simple log scaling factor of reference profile *******
             np = 1
             do i=1,np
-             xn(ix+i)=x0(ix+i)+err(ix+i)*gasdev(idum)
+             xn(ix+i)=x0(ix+i)+err(ix+i)*gasdev1(idum)
             enddo
           elseif(varident(ivar,3).eq.4)then
 C         ******** profile held as deep amount, fsh and variable knee press
             np = 3
             do i=1,np
-             xn(ix+i)=x0(ix+i)+err(ix+i)*gasdev(idum)
+             xn(ix+i)=x0(ix+i)+err(ix+i)*gasdev1(idum)
             enddo
           elseif(varident(ivar,3).eq.6)then
 C         ******** profile cloud od at specified level plus fsh.
             np = 2
             do i=1,np
-             xn(ix+i)=x0(ix+i)+err(ix+i)*gasdev(idum)
+             xn(ix+i)=x0(ix+i)+err(ix+i)*gasdev1(idum)
             enddo
           elseif(varident(ivar,3).eq.666)then  
 C         **** Pressure at given altitude *******
             np = 1
             do i=1,np
-             xn(ix+i)=x0(ix+i)+err(ix+i)*gasdev(idum)
+             xn(ix+i)=x0(ix+i)+err(ix+i)*gasdev1(idum)
             enddo
           elseif(varident(ivar,3).eq.777)then  
 C         **** Tangent height correction *******
             np = 1
             do i=1,np
-             xn(ix+i)=x0(ix+i)+err(ix+i)*gasdev(idum)
+             xn(ix+i)=x0(ix+i)+err(ix+i)*gasdev1(idum)
             enddo
           elseif(varident(ivar,3).eq.999)then  
 C         **** Surface temperature *******
             np = 1
             do i=1,np
-             xn(ix+i)=x0(ix+i)+err(ix+i)*gasdev(idum)
+             xn(ix+i)=x0(ix+i)+err(ix+i)*gasdev1(idum)
             enddo
           elseif(varident(ivar,3).eq.555)then  
 C         **** Radius of planet *******
             np = 1
             do i=1,np
-             xn(ix+i)=x0(ix+i)+err(ix+i)*gasdev(idum)
+             xn(ix+i)=x0(ix+i)+err(ix+i)*gasdev1(idum)
             enddo
           endif
 

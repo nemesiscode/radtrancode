adb5c9b637e2f6f5bdae8950b0d2fa7f5aa1e7da
Perf. Patrick Irwin <patrick.irwin@physics.ox.ac.uk>
2013-07-30 16:01:56 +0000

    New check routines to guard against iterations going non-physical.
    git-svn-id: https://scm.physics.ox.ac.uk/svn/radtrancode/trunk@367 4dc0e162-f54d-4b9e-8a18-81c7a7f2ddce

---
+++ nemesis/check_iteration.f
@@ -0,0 +1,31 @@
+      subroutine check_iteration(nvar,varident,xn,npro,icheck)
+      
+      include '../radtran/includes/arrdef.f'
+      include 'arraylen.f'
+      integer nvar,nx,varident(mvar,3),icheck
+      real xn(mx),tmp(maxpro)
+      integer np,npvar,istart,logflag
+
+      icheck=0
+      istart=0
+      do 1000 ivar=1,nvar
+       imod=varident(ivar,3)
+       itype=varident(ivar,1)
+       np = npvar(imod,npro)
+       do i=1,np
+        tmp(i)=xn(i+istart)
+       enddo
+C      See if this is a temperature model. If temperatures gone negative
+C      then send error flag.
+       if(itype eq 0) then 
+         do i=1,np
+          ilog=logflag(itype,imod,ip)
+          if(ilog.eq.0.and.tmp(i).lt.0.)icheck=1
+         enddo
+       endif
+       istart=istart+np
+1000  continue
+ 
+      return
+
+      end
---
+++ nemesis/check_profile.f
@@ -0,0 +1,46 @@
+      integer function check_profile(runname)
+C     ****************************************************************
+C     Code to read in latest prf file and see if it has non-negative 
+C     temperatures and vmrs.
+C
+C     ****************************************************************
+
+      implicit none
+      INCLUDE '../radtran/includes/arrdef.f'
+      INCLUDE '../radtran/includes/pathcom.f'
+C ../includes/pathcom.f holds the variables used by the software when
+C calculating atmospheric paths (e.g. NPATH and IMOD).
+      INCLUDE '../radtran/includes/laycom.f'
+      integer i,j,icheck
+      real sum
+      character*100 runname,ipfile
+
+      call file(runname,ipfile,'prf')
+      call rdmod(ipfile)
+
+      ipfile='aerosol.prf'
+      call rddmod(ipfile)
+
+      print*,'Check_profile called' 
+      icheck=0
+      do 10 i= 1,npro
+       if(T(i).lt.0.0) icheck=1
+       print*,T(i),(vmr(i,j),j=1,nvmr)
+       sum=0.
+       do 20 j=1,nvmr
+        if (vmr(i,j).lt.0.0) icheck=1
+        sum=sum+vmr(i,j)
+20     continue
+       print*,'Summed vmrs = ',sum
+       print*,(dust(j,i),j=1,ncont)
+       do 30 j=1,ncont
+        if(dust(j,i).lt.0.0) icheck=1
+30     continue
+10    continue
+
+
+      check_profile=icheck
+
+      return
+
+      end

17afbbb95ca9b4a5fa9ac3f394400f83753521cd
Perf. Patrick Irwin <patrick.irwin@physics.ox.ac.uk>
2013-08-02 10:19:04 +0000

    Updated solardat common block to include IFORM
    git-svn-id: https://scm.physics.ox.ac.uk/svn/radtrancode/trunk@381 4dc0e162-f54d-4b9e-8a18-81c7a7f2ddce

--- radtran/cirsrad/get_solar_wave.f
+++ radtran/cirsrad/get_solar_wave.f
@@ -17,11 +17,11 @@ C-----------------------------------------------------------------------
         include '../includes/arrdef.f'
         include '../includes/constdef.f'
 
-	INTEGER		npt,iread
+	INTEGER		npt,iread,iform
 	REAL		x, dist, solar, wave(maxbin), rad(maxbin), y
         REAL		area, solrad
 
-        common/solardat/iread, solrad, wave, rad, npt
+        common/solardat/iread, iform, solrad, wave, rad, npt
 
         if(iread.ne.999)then
          print*,'GET_SOLAR_WAVE: error. No solar spectrum initialised'
@@ -30,13 +30,12 @@ C-----------------------------------------------------------------------
 
 	call interp(wave, rad, npt, y, x)
 
-C       If dist is > 0 then this must be the solar spectrum for a solar
-C       system planet calculation. Hence we need to divide by 4*pi*dist^2
+C       If dist is > 0 then we calculate the flux at the distance of the 
+C       planet from the star. Hence we need to divide by 4*pi*dist^2
 C       to provide solar flux in units of W cm-2 um-1 or W cm-2 (cm-1)-1.
 C      
-C       If dist is < 0, then this must be a stellar flux for secondary
-C       transit calculation and we should leave it alone in units of
-C       W um-1 or W (cm-1)-1
+C       If dist is < 0, then we want the total spectral irradiance
+C       in units of W um-1 or W (cm-1)-1
        
         if(dist.gt.0.)then
           area = 4*PI*(dist*AU)**2

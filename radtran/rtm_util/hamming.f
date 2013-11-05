c2fb39e139a4a33d9b137ed30176e03394a3629b
Perf. Patrick Irwin <patrick.irwin@physics.ox.ac.uk>
2013-11-05 11:11:10 +0000

    Updated Hamming Function.
    git-svn-id: https://scm.physics.ox.ac.uk/svn/radtrancode/trunk@432 4dc0e162-f54d-4b9e-8a18-81c7a7f2ddce

--- radtran/rtm_util/hamming.f
+++ radtran/rtm_util/hamming.f
@@ -1,30 +1,38 @@
-      real function hamming(FWHM,xoff)
+      real function hamming(FWHM,k)
 C     ***********************************************************************************
-C     Function to compute Hamming Function from parameters defined in Wikipedia
+C     Function to compute Hamming Function from parameters defined in
+C     Wolfram MathWorld
+C
+C     Adapted from grid_hamm.pro, written by Nick Teanby
 C
 C     Input parameters
-C 	FWHM	FWHM of Hamming function
-C	xoff	offset from centre of Hamming Function in units of FWHM
+C 	FWHM	real	FWHM of Hamming function
+C	k	real	offset from centre of Hamming Function in same 
+C			units as the FWHM
 C    
 C     Output parameter
-C	hamming	Hamming Function weight
+C	hamming	real	Hamming Function weight
 C
 C     Pat Irwin		5/11/13
 C
 C     ***********************************************************************************
       implicit none
-      real pi,FWHM,alpha,beta,wfwhm,xx,xoff
-      parameter (pi=3.1415927, alpha=0.53836, beta=0.46164)
-      parameter (wfwhm = 0.5264)
+      real pi,FWHM,a,k,stest
+      parameter (pi=3.1415927)
       
+      a = 0.9077/fwhm
+      stest = 1.0 - 4.0*a*a*k*k
 
-C     Compute position of xoff in scaled window covering range 0 to 1.
-      xx = 0.5 + 0.5*wfwhm*abs(xoff)/(0.5*FWHM)
-
-      if(xx.le.1.0) then
-       hamming = alpha-beta*cos(2.*pi*xx)
+      if (abs(stest).gt.1.0e-6) then
+        if (k.ne.0.0) then
+         hamming=a*(1.08-0.64*a*a*k*k)*sin(2.0*pi*a*k)/
+     &         ((1.0-4.0*a*a*k*k)*(2.0*pi*a*k))
+        else
+         hamming=a*1.08
+        endif
       else
-       hamming=0.
+C       sin(..... converges to 0.5 close to (1.0-4.0*a*a*k*k)=0
+        hamming=a*(1.08-0.64*a*a*k*k)*0.5
       endif
 
       return

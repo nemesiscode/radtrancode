e70eab371853c4c2ec5ee3474252b4e829d5fd4c
Perf. Patrick Irwin <patrick.irwin@physics.ox.ac.uk>
2014-04-08 15:07:58 +0000

    Updated for new .prf file format.
    git-svn-id: https://scm.physics.ox.ac.uk/svn/radtrancode/trunk@571 4dc0e162-f54d-4b9e-8a18-81c7a7f2ddce

--- radtran/monteck/readprof.f
+++ radtran/monteck/readprof.f
@@ -23,7 +23,7 @@ C
 C     *************************************************************
       IMPLICIT NONE
       INCLUDE '../includes/arrdef.f'
-      INTEGER NPRO,NGAS,IFORM,N,I,J,K
+      INTEGER NPRO,NGAS,AMFORM,N,I,J,K
       INTEGER IPLANET
 
       REAL P(MAXPRO),T(MAXPRO),H(MAXPRO),VMR(MAXPRO,MAXGAS)
@@ -43,37 +43,20 @@ C     First skip header
 1     FORMAT(A)
 54    READ(1,1)BUFFER
       IF(BUFFER(1:1).EQ.'#') GOTO 54
-      READ(BUFFER,*)IFORM
-      READ(1,*)IPLANET,LATITUDE,NPRO,NGAS,MOLWT
+      READ(BUFFER,*)AMFORM
+      IF(AMFORM.EQ.0)THEN
+        READ(1,*)IPLANET,LATITUDE,NPRO,NGAS,MOLWT
+      ELSE
+        READ(1,*)IPLANET,LATITUDE,NPRO,NGAS
+      ENDIF
       DO 20 I=1,NGAS
        READ(1,*)ID(I),ISO(I)
 20    CONTINUE
-C     reading the first block of profiles
+C     Skip header
       READ(1,*)
-      N=MIN(NGAS,3)
-C     N is the maximum VMR which can be read in from the next block
       DO 30 I=1,NPRO
-       IF(IFORM.EQ.0)THEN
-         READ(1,*)H(I),P(I),T(I),(VMR(I,J),J=1,N)
-       ELSE IF(IFORM.EQ.1)THEN
-         READ(1,*)H(I),T(I),(VMR(I,J),J=1,N)
-       ELSE   
-         CALL WTEXT('invalid format')
-         STOP   
-       END IF
+         READ(1,*)H(I),P(I),T(I),(VMR(I,J),J=1,NGAS)
 30    CONTINUE
-C     reading in additional blocks if any
-C     N VMR profiles have been read in so far
-33    IF(NGAS.GT.N)THEN
-        READ(1,*)
-C       profiles up to VMR(?,K) to be read from this block
-        K=MIN(NGAS,(N+6))
-        DO 32 I=1,NPRO
-         READ(1,*)(VMR(I,J),J=N+1,K)
-32      CONTINUE
-        N=K
-        GOTO 33
-      END IF
 
       CLOSE(UNIT=1)
 

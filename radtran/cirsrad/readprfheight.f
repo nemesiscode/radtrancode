8a62900d2bfd49fe2b2e87f8f84a1973143b8612
Perf. Patrick Irwin <patrick.irwin@physics.ox.ac.uk>
2014-06-06 13:56:51 +0000

    New routine to read in the height array from the .prf file.
    git-svn-id: https://scm.physics.ox.ac.uk/svn/radtrancode/trunk@626 4dc0e162-f54d-4b9e-8a18-81c7a7f2ddce

---
+++ radtran/cirsrad/readprfheight.f
@@ -0,0 +1,56 @@
+      SUBROUTINE READPRFHEIGHT(IPFILE,NPRO,HEIGHT)
+C     $Id:
+C     ***********************************************************************
+C     Subroutine to read a .prf file and return the number of vertical
+C     levels and the height array.
+C
+C     Input filename
+C	IPFILE	CHARACTER*100 	Input filename
+C
+C     Output filename
+C    	NPRO	INTEGER		Number of levels
+C	HEIGHT(NPRO)	REAL	Heights
+C
+C     Pat Irwin 17/10/03	Original
+C     Pat Irwin 4/6/14		Modified from readrefhead.f
+C
+C     ***********************************************************************
+      IMPLICIT NONE
+      include '../includes/arrdef.f'
+
+      CHARACTER*100 IPFILE,BUFFER
+      CHARACTER*8 PNAME
+      INTEGER NPRO,NVMR,AMFORM,IPLANET,ID(MAXGAS),ISO(MAXGAS),I
+      REAL HEIGHT(MAXPRO),P(MAXPRO),T(MAXPRO)
+      REAL LATITUDE,MOLWT
+      LOGICAL GASGIANT
+
+      CALL FILE(IPFILE,IPFILE,'prf')
+      
+      print*,'Reading reference file from : ',IPFILE
+      OPEN(UNIT=1,FILE=IPFILE,STATUS='OLD')
+C     First skip header
+54     READ(1,1)BUFFER
+       IF(BUFFER(1:1).EQ.'#') GOTO 54
+       READ(BUFFER,*)AMFORM
+1      FORMAT(A)
+       IF(AMFORM.EQ.1)THEN
+        READ(1,*)IPLANET,LATITUDE,NPRO,NVMR
+       ELSE
+        READ(1,*)IPLANET,LATITUDE,NPRO,NVMR,MOLWT
+       ENDIF
+       DO 20 I=1,NVMR
+         READ(1,*)ID(I),ISO(I)
+20     CONTINUE
+C      Skip header
+       READ(1,*)
+       DO 30 I=1,NPRO
+         READ(1,*)HEIGHT(I),P(I),T(I)
+30     CONTINUE
+
+      CLOSE(UNIT=1)
+
+      RETURN
+
+      END
+

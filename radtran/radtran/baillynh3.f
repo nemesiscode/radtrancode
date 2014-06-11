8b8b83990fe21b5597d398770064750abf52ca76
Rohini Giles <giles@atm.ox.ac.uk>
2014-06-11 09:00:42 +0000

    Changed the correction factor after corresponding with author
    git-svn-id: https://scm.physics.ox.ac.uk/svn/radtrancode/trunk@659 4dc0e162-f54d-4b9e-8a18-81c7a7f2ddce

--- radtran/radtran/baillynh3.f
+++ radtran/radtran/baillynh3.f
@@ -5,28 +5,28 @@ C
 C  PURPOSE        COMPUTE NH3 VOIGT LINESHAPE MODIFIED FOR 
 C                 SUB-LORENTZIAN LINE WINGS
 C
-C		  Modified from raw Bailly (2004) paper
+C		  Modified from Bailly (2004) paper
 C
 C  VERSION        1.0	R.S. GILES	06/06/2014
 C
 C***********************************************************************
       
-      REAL FUNCTION BAILLYNH3(X,Y,DV1)
+      REAL FUNCTION BAILLYNH3(X,Y,DV)
       IMPLICIT NONE
 
-      REAL X,Y,DV1,CHI,HUMLIC0,HUMLIC
+      REAL X,Y,DV,DV1,CHI,HUMLIC0,HUMLIC
 
       HUMLIC0=HUMLIC(X,Y)
+      DV1=ABS(DV)
 
       IF (DV1.LT.10.0) THEN
-           CHI = 1.0
-C          CHI = 1.0*EXP(-DV1*0.00002432)
+           CHI = 1.0*EXP(-DV*0.00243)
       ENDIF
       IF (DV1.GE.10.0.AND.DV1.LT.65.0) THEN
-           CHI = 1.42*EXP(-DV1*0.0354)
+           CHI = 1.42*EXP(-DV1*0.0354)*EXP(-DV*0.00243)
       ENDIF
       IF (DV1.GE.65.0) THEN
-	   CHI = 0.172*EXP(-DV1*0.00296)
+	   CHI = 0.172*EXP(-DV1*0.00296)*EXP(-DV*0.00243)
       ENDIF
 
       BAILLYNH3=CHI*HUMLIC0

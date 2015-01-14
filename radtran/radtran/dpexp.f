3ad5c00601d684a1d7355eaf30ca15b64d2fb127
Ryan Garland <ryan.garland@physics.ox.ac.uk>
2015-01-14 15:57:03 +0000

    Limited maximum value of x in exp(x) so that overflow is avoided.
    git-svn-id: https://scm.physics.ox.ac.uk/svn/radtrancode/trunk@752 4dc0e162-f54d-4b9e-8a18-81c7a7f2ddce

--- radtran/radtran/dpexp.f
+++ radtran/radtran/dpexp.f
@@ -6,6 +6,9 @@ C     overflow instead of underflow so can't be just ignored.
 C     Has no useful effect under VMS since exponent range is the same for
 C     double and single precision and the error is an underflow anyway.
       Y = MAX(X,-500.0)
+      IF(Y.GE.88.7)THEN
+       Y=88.7
+      ENDIF
       DPEXP=SNGL(DEXP(DBLE(Y)))
       RETURN
       END

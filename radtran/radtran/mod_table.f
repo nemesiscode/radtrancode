730ef99d26b4623c48948ad7e35a6f078ade52f2
Perf. Patrick Irwin <patrick.irwin@physics.ox.ac.uk>
2015-05-13 09:16:42 +0000

    Program to change the gas ID of a k-table. Program necessary as the gas IDs of recently added gases have got a little muddled of late. A proposed numbering scheme has been added to SVN recently and this program allows one to retrospectivel re-identify a k-table.
    git-svn-id: https://scm.physics.ox.ac.uk/svn/radtrancode/trunk@822 4dc0e162-f54d-4b9e-8a18-81c7a7f2ddce

---
+++ radtran/radtran/mod_table.f
@@ -0,0 +1,137 @@
+      PROGRAM MOD_TABLE
+C     $Id: cut_table.f,v 1.1 2011-06-17 15:25:43 irwin Exp $
+C***********************************************************************
+C_TITL:	CUT_TABLE.f
+C
+C_DESC: Changes gas ID of a k-table
+C
+C_ARGS:	See the definitions below.
+C
+C_FILE:	UNIT=LUN	output file
+C	unit=lun0
+C
+C_CALL:	file
+C	remsp
+C	pindex	
+C
+C_HIST:	10feb95	PGJI	ORIGINAL VERSION
+C	13jan11 PGJI	Modified to cut data
+C***************************** VARIABLES *******************************
+
+      INCLUDE '../includes/arrdef.f'
+      INCLUDE '../includes/bincom.f'
+      INCLUDE '../includes/dbcom.f'
+C NOTE: dbcom defines the linedata base variables. it is not normally
+C stored in the same directory as the rest of the code
+      INCLUDE '../includes/pathcom.f'
+
+      INTEGER LUN,LUN0,LUN1      
+      PARAMETER (LUN=2,LUN0=30,LUN1=31)
+C MAXOUT the maximum number of output points
+
+      INTEGER PINDEX,CP,CT,LOOP,I1,I2
+
+      INTEGER IREC,IREC0
+      REAL P1,T1,PRESS1(MAXK),TEMP1(MAXK),VCEN(MAXBIN),X1,X2
+      REAL G_ORD(MAXG),K_G(MAXG),DEL_G(MAXG),KVAL
+
+      CHARACTER*100 KTAFIL,OPFILE1,OPFILE2,OUTFIL
+      CHARACTER*1 ANS
+C******************************** CODE *********************************
+
+      CALL PROMPT('Enter input filename : ')
+      READ(5,23)OPFILE1
+23    FORMAT(A)
+      CALL FILE(OPFILE1,KTAFIL,'kta')
+
+      CALL PROMPT('Enter output filename : ')
+      READ(5,23)OPFILE2
+      CALL FILE(OPFILE2,OUTFIL,'kta')
+
+
+C     Assume 4-byte words per record
+      NW=1
+
+      IRECL = NW*ISYS()
+      WRITE(*,*)'irecl = ',irecl
+
+      OPEN(UNIT=LUN0,FILE=KTAFIL,STATUS='OLD',ACCESS='DIRECT',
+     1 RECL=IRECL)
+
+      OPEN(UNIT=LUN1,FILE=OUTFIL,STATUS='UNKNOWN',ACCESS='DIRECT',
+     1 RECL=IRECL)
+
+
+      READ(LUN0,REC=1)IREC0
+      READ(LUN0,REC=2)NPOINT
+      READ(LUN0,REC=3)VMIN
+      READ(LUN0,REC=4)DELV
+      READ(LUN0,REC=5)FWHM
+      READ(LUN0,REC=6)NP
+      READ(LUN0,REC=7)NT
+      READ(LUN0,REC=8)NG
+      READ(LUN0,REC=9)IDGAS(1)
+      READ(LUN0,REC=10)ISOGAS(1)
+
+
+      print*,'Current gas ID,ISO = ',IDGAS(1),ISOGAS(1)
+      print*,'Enter new values : '
+      read*, I1,I2
+      IDGAS(1)=I1
+      ISOGAS(1)=I2
+
+
+      WRITE(LUN1,REC=1)IREC0
+      WRITE(LUN1,REC=2)NPOINT
+      WRITE(LUN1,REC=3)VMIN
+      WRITE(LUN1,REC=4)DELV
+      WRITE(LUN1,REC=5)FWHM
+      WRITE(LUN1,REC=6)NP
+      WRITE(LUN1,REC=7)NT
+      WRITE(LUN1,REC=8)NG
+      WRITE(LUN1,REC=9)IDGAS(1)
+      WRITE(LUN1,REC=10)ISOGAS(1)
+
+      IREC = 11
+      DO 299 J=1,NG
+        READ(LUN0,REC=IREC)G_ORD(J)
+        WRITE(LUN1,REC=IREC)G_ORD(J)
+        IREC = IREC + 1
+299   CONTINUE
+      DO 399 J=1,NG
+        READ(LUN0,REC=IREC)DEL_G(J)
+        WRITE(LUN1,REC=IREC)DEL_G(J)
+        IREC=IREC+1
+399   CONTINUE
+      IREC = 11 + 2*NG + 2
+      DO 301 J=1,NP
+        READ(LUN0,REC=IREC)PRESS1(J)
+        WRITE(LUN1,REC=IREC)PRESS1(J)
+        IREC = IREC + 1
+301   CONTINUE
+      DO 302 J=1,NT
+        READ(LUN0,REC=IREC)TEMP1(J)
+        WRITE(LUN1,REC=IREC)TEMP1(J)
+        IREC = IREC + 1
+302   CONTINUE
+
+
+
+     IREC = IREC0
+
+      DO 10 I=1,NPOINT
+       DO 20 J=1,NP
+        DO 30 K=1,NT
+          DO 40 LOOP=1,NG
+            READ(LUN0,REC=IREC)KVAL
+            WRITE(LUN1,REC=IREC)KVAL
+            IREC = IREC + 1
+40        CONTINUE
+30      CONTINUE
+20     CONTINUE
+10    CONTINUE
+
+      CLOSE(LUN0)
+      CLOSE(LUN1)
+
+      END

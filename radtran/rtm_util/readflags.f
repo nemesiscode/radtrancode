fa04ee3a162b7fb35e46449dea161ee56532cef2
Perf. Patrick Irwin <patrick.irwin@physics.ox.ac.uk>
2015-10-07 16:18:09 +0000

    Updated for new NH3 visible continuum.
    git-svn-id: https://scm.physics.ox.ac.uk/svn/radtrancode/trunk@894 4dc0e162-f54d-4b9e-8a18-81c7a7f2ddce

--- radtran/rtm_util/readflags.f
+++ radtran/rtm_util/readflags.f
@@ -1,5 +1,5 @@
       subroutine readflags(runname,inormal,iray,ih2o,ich4,io3,
-     &  iptf,imie)
+     &  inh3,iptf,imie)
 C     ********************************************************************
 C     Subroutine to read in the runname.fla file, which contains a number
 C     of spectral processing flags.
@@ -13,6 +13,7 @@ C	iray	integer	Rayleigh optical depth off(0) or on(1)
 C	ih2o	integer	Include H2O continuum (1)
 C	ich4	integer	Include Karkoschka methane continuum (1)
 C	io3	integer	Include Bass UV ozone absorption (1)
+C	inh3	integer	Include Lutz and Owen ammonia continuum
 C	iptf	integer	Use modified CH4 high-T part. func. (1)
 C       imie	integer	Use PHASEN.DAT phase file (1) or hgphaseN.dat phase
 C			file otherwise
@@ -21,7 +22,7 @@ C     Pat Irwin	21/2/12
 C
 C     ********************************************************************
       character*(*) runname
-      integer inormal,iray,ih2o,ich4,io3,iptf,imie
+      integer inormal,iray,ih2o,ich4,io3,iptf,imie,inh3
 
       call file(runname,runname,'fla')
 
@@ -31,6 +32,7 @@ C     ********************************************************************
       read(12,*)ih2o
       read(12,*)ich4
       read(12,*)io3
+      read(12,*)inh3
       read(12,*)iptf
       read(12,*)imie
       close(12)

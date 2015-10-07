3646076fb81e8608e8112fb1ca670a37774ff0f8
Perf. Patrick Irwin <patrick.irwin@physics.ox.ac.uk>
2015-10-07 16:12:22 +0000

    Updated to deal with numerical instability at ig=ng for ranking.
    git-svn-id: https://scm.physics.ox.ac.uk/svn/radtrancode/trunk@888 4dc0e162-f54d-4b9e-8a18-81c7a7f2ddce

--- radtran/cirsrad/rank.f
+++ radtran/cirsrad/rank.f
@@ -28,7 +28,13 @@ C-----------------------------------------------------------------------
 	do I = 1, ng
 		g_ord(I+1) = g_ord(I) + gw(I)
 	end do
+C       Make sure g_ord(ng+1)=1. (rounding errors can lead to numbers just
+C                                 less than 1.0)
         if(g_ord(ng+1).lt.1.)g_ord(ng+1)=1.
+
+C        do i=1,ng+1
+C         print*,i,g_ord(i)
+C        enddo
 
 	call sort2(nloop, cont, weight)
 
@@ -41,11 +47,18 @@ C	interpolation is used to deal with steps crossing over the
 C	required g intervals.
 C
 C-----------------------------------------------------------------------
+C        print*,'sort2'
+C        do i=1,nloop
+C         print*,i,cont(i),weight(i)
+C        enddo
 
         gdist(0) = 0.
 	gdist(1) = weight(1)
+C        print*,gdist(0)
+C        print*,gdist(1)
 	do I = 2, nloop
 		gdist(I) = weight(I) + gdist(I-1) 
+C                print*,gdist(i)
 	enddo
 
 	do I = 1, ng
@@ -55,9 +68,11 @@ C-----------------------------------------------------------------------
 	ig = 1
 	sum=0.
 	do I = 1, nloop
-		if(gdist(I).lt.g_ord(ig+1))then
+C                print*,'C',i,gdist(i),g_ord(ig+1)
+		if(gdist(I).lt.g_ord(ig+1).and.ig.le.ng)then
 			k_g(ig) = k_g(ig) + cont(I) * weight(I)
 			sum = sum + weight(I)
+C                        print*,'A',ig,k_g(ig),sum
 		else
 			frac = (g_ord(ig+1)-gdist(I-1))/
      1				(gdist(I)-gdist(I-1))
@@ -68,6 +83,7 @@ C-----------------------------------------------------------------------
 			sum = (1.-frac) * weight(I)
 			k_g(ig) = k_g(ig) + (1.-frac)*cont(I)
      1				*weight(I)
+C                        print*,'B',ig,k_g(ig),sum
 		endif
 	enddo
 	if (ig.eq.ng) k_g(ig) = k_g(ig)/sum

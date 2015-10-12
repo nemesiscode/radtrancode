359afe55cb5497d720c3208564c89da6b9fd1470
Joanna Barstow <joannakbarstow@cantab.net>
2015-10-12 15:27:25 +0000

    Added definition of flag INH3, which was missing and causing compilation bugs
    git-svn-id: https://scm.physics.ox.ac.uk/svn/radtrancode/trunk@905 4dc0e162-f54d-4b9e-8a18-81c7a7f2ddce

--- radtran/includes/gascom.f
+++ radtran/includes/gascom.f
@@ -1,6 +1,6 @@
 C     Define the fixed GASCON parameters
-      INTEGER ICH4,IH2O,IO3
+      INTEGER ICH4,IH2O,IO3, INH3
 C     ----------------------------------------------------------------------
-      COMMON /GASCONDEF/ICH4,IH2O,IO3
+      COMMON /GASCONDEF/ICH4,IH2O,IO3, INH3
 C     ----------------------------------------------------------------------
 

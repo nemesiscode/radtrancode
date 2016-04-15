c0b4f552a5f712bc2a8f130b7a7e4e5f2a633bfe
Perf. Patrick Irwin <patrick.irwin@physics.ox.ac.uk>
2016-04-15 16:09:43 +0000

    Updated empirical CO2 cia coefficients.
    git-svn-id: https://scm.physics.ox.ac.uk/svn/radtrancode/trunk@951 4dc0e162-f54d-4b9e-8a18-81c7a7f2ddce

--- radtran/radtran/co2cia.f
+++ radtran/radtran/co2cia.f
@@ -16,8 +16,19 @@ C     2.3 micron window. Assume de Bergh 1995 a = 4e-8 cm-1/amagat^2
 C     1.73 micron window. Assume mean a = 6e-9 cm-1/amagat^2
       if(xl.ge.1.7.and.xl.le.1.76)aco2 = 6e-9
 
+C     1.28 micron window. Update from Federova et al. (2014) to
+C     aco2 = 1.5e-9 cm-1/amagat^2
+      if(xl.ge.1.25.and.xl.lt.1.35)aco2 = 1.5e-9
+
 C     1.18 micron window. Assume a mean a = 1.5e-9 cm-1/amagat^2
-      if(xl.ge.1.05.and.xl.le.1.35)aco2 = 1.5e-9
+C      if(xl.ge.1.05.and.xl.le.1.35)aco2 = 1.5e-9
+C     Update from Federova et al. (2014)
+      if(xl.ge.1.125.and.xl.le.1.225)aco2 = 0.5*(0.31+0.79)*1e-9
+
+C     1.10 micron window. Update from Federova et al. (2014) to
+C     aco2 = 0.5*(0.29+0.67)*1e-9 cm-1/amagat^2
+      if(xl.ge.1.06.and.xl.lt.1.125)aco2 = 0.5*(0.29+0.67)*1e-9
+
 
       co2cia = aco2
 

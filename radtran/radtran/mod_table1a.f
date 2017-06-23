e6fc5f0b23f26f19d6c9c724b60852518968d80b
Patrick Irwin <patrick.irwin@physics.ox.ac.uk>
2017-06-23 11:25:46 +0100

    New k-table modification files

---
+++ radtran/radtran/mod_table1a.f
@@ -0,0 +1,89 @@
+      PROGRAM MOD_TABLE1A
+C     $Id:
+C***********************************************************************
+C_TITL:	CUT_TABLE.f
+C
+C_DESC: Changes k-table from channel to continuous
+C
+C_ARGS:	See the definitions below.
+C
+C_FILE:	UNIT=LUN	output file
+C	unit=lun0
+C
+C_CALL:	file
+C	remsp
+C	pindex	
+C
+C_HIST:	10feb95	PGJI	ORIGINAL VERSION
+C	13jan11 PGJI	Modified to cut data
+C***************************** VARIABLES *******************************
+
+      INCLUDE '../includes/arrdef.f'
+      INCLUDE '../includes/bincom.f'
+      INCLUDE '../includes/dbcom.f'
+C NOTE: dbcom defines the linedata base variables. it is not normally
+C stored in the same directory as the rest of the code
+      INCLUDE '../includes/pathcom.f'
+
+      INTEGER LUN,LUN0,LUN1,IX
+      PARAMETER (LUN=2,LUN0=30,LUN1=31)
+C MAXOUT the maximum number of output points
+
+      INTEGER PINDEX,CP,CT,LOOP,I1,I2
+
+      INTEGER IREC,IREC0,IREC1,IREC01
+      REAL P1,T1,PRESS1(MAXK),TEMP1(MAXK),VCEN(MAXBIN),X1,X2
+      REAL G_ORD(MAXG),K_G(MAXG),DEL_G(MAXG),KVAL
+
+      CHARACTER*100 KTAFIL,OPFILE1,OPFILE2,OUTFIL
+      CHARACTER*1 ANS
+C******************************** CODE *********************************
+
+      CALL PROMPT('Enter input filename : ')
+      READ(5,23)OPFILE1
+23    FORMAT(A)
+      CALL FILE(OPFILE1,KTAFIL,'kta')
+
+
+C     Assume 4-byte words per record
+      NW=1
+
+      IRECL = NW*ISYS()
+      WRITE(*,*)'irecl = ',irecl
+
+      OPEN(UNIT=LUN0,FILE=KTAFIL,STATUS='OLD',ACCESS='DIRECT',
+     1 RECL=IRECL)
+
+
+      READ(LUN0,REC=1)IREC0
+      READ(LUN0,REC=2)NPOINT
+      READ(LUN0,REC=3)VMIN
+      READ(LUN0,REC=4)DELV
+      READ(LUN0,REC=5)FWHM
+      READ(LUN0,REC=6)NP
+      READ(LUN0,REC=7)NT
+      READ(LUN0,REC=8)NG
+      READ(LUN0,REC=9)IDGAS(1)
+      READ(LUN0,REC=10)ISOGAS(1)
+
+      IF(DELV.GT.0)THEN
+       IX=1
+      ELSE
+       IX=0
+      ENDIF
+      IF(IX.EQ.0)THEN
+       PRINT*,'DELV = ',DELV
+       PRINT*,'Enter new DELV'
+       READ*,DELV
+       PRINT*,'Enter new FWHM'
+       READ*,FWHM
+
+       WRITE(LUN0,REC=4)DELV
+       WRITE(LUN0,REC=5)FWHM
+      ELSE
+       PRINT*,'Cannot modify this file. Use Mod_table1 instead'
+      ENDIF
+
+      CLOSE(LUN0)
+
+      END

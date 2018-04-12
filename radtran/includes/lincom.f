9f3826bc706a690ce0d6f5ce8d6d8dc9b55611cf
Patrick Irwin <patrick.irwin@physics.ox.ac.uk>
2018-04-12 14:54:03 +0100

    Updated subroutines to allow for fact that wavenumber is now passed to line database codes as double precision rather than real.

--- radtran/includes/lincom.f
+++ radtran/includes/lincom.f
@@ -22,8 +22,8 @@ C MAXLIN: maximum number of lines which can be stored.
       INTEGER IDLIN(MAXLIN),LINMAX,NXTLIN
 C IDLIN: Air Force Geospace Lab. identifier.
 
-      DOUBLE PRECISION SLIN(MAXLIN)
-      REAL VLIN(MAXLIN),ALIN(MAXLIN),PSHIFT(MAXLIN)
+      DOUBLE PRECISION SLIN(MAXLIN),VLIN(MAXLIN)
+      REAL ALIN(MAXLIN),PSHIFT(MAXLIN)
 C VLIN: Line position [cm^-1].
 C SLIN: Line strength [cm^-1 molecule^-1 cm^-2] at STP.
 C ALIN: Lorentz half width [cm^-1] at STP.

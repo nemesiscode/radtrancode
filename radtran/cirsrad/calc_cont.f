ae01c1a05b6f5e53fd79537f6a617a208dec8172
Patrick Irwin <patrick.irwin@physics.ox.ac.uk>
2018-04-20 16:32:36 +0100

    Added additional line continuum contribution for lines stripped from line databases that are too large to handle.

--- radtran/cirsrad/calc_cont.f
+++ radtran/cirsrad/calc_cont.f
@@ -1,5 +1,5 @@
       SUBROUTINE CALC_CONT(WING,MAXDV,NLAYER,NGAS,PRESS,TEMP,
-     1 FRAC,IDGAS,ISOGAS,IPROC,IB)
+     1 FRAC,IDGAS,ISOGAS,IPROC,IB,IX)
 C     $Id:
 C***********************************************************************
 C_TITL:	CALC_CONT.f
@@ -18,6 +18,7 @@ C	IDGAS(NGAS)	INTEGER	Gas ID
 C       ISOGAS(NGAS)	INTEGER Isotope ID
 C	IPROC(NGAS)	INTEGER	Line wing processing parameter.
 C	IB		INTEGER Buffer 1 or 2
+C	IX		INTEGER Call number to routine
 C
 C	../includes/*.f variables:
 C	VLIN(2,MAXLIN)	REAL*8	Line position [cm^-1].
@@ -53,7 +54,7 @@ C***************************** VARIABLES *******************************
       INCLUDE '../includes/arrdef.f'
 C     Continuum and temperature parameter variables ...
       INCLUDE '../includes/contdef.f'
-      REAL TRATIO,TSTIM
+      REAL TRATIO,TSTIM,VOUT(MAXBIN),YOUT(MAXBIN)
       REAL FRAC(MAXLAY,MAXGAS),PRESS(NLAYER),TEMP(NLAYER)
 
       INCLUDE '../includes/lincomc.f'
@@ -62,18 +63,22 @@ C VLIN, SLIN, ALIN, ELIN, SBLIN, TDW, TDWS and that lot).
 
 
 C     General variables ...
-      REAL DV,LINECONTRIB,VV,X,FNH3,FH2
+      REAL DV,LINECONTRIB,X,FNH3,FH2
       INTEGER I,J,K,L,LINE,IBIN,CURBIN,IGAS,IB
-      INTEGER LAYER
+      INTEGER LAYER,IX
+      DOUBLE PRECISION VV,VJ,STR
+      REAL LSE,XWIDA,XWIDS,XTDEP
+
       REAL DPEXP,ABSCO,Y,CONVAL
        
 C     GASCON switches
       INCLUDE '../includes/gascom.f'
+      INCLUDE '../includes/lcocom.f'
 
 C******************************** CODE *********************************
 
 
-      
+      print*,'CALC_CONT. IX,IB,NLAYER = ',IX,IB, NLAYER      
       DO 13 LINE=1,NLINE(IB)
        CURBIN = 1 + INT((VLIN(IB,LINE)-VBIN(1))/WING)
        IGAS=IDLIN(IB,LINE)
@@ -83,8 +88,8 @@ C       Computing continuum for all except adjacent bins
         IF(ABS(CURBIN-J).LE.1)GOTO 15
 
         DO 21 K=1,IORDP1
-          VV = CONWAV(K) + VBIN(J) 
-          DV = (VV - VLIN(IB,LINE))
+          VV = DBLE(CONWAV(K) + VBIN(J)) 
+          DV = SNGL(VV - VLIN(IB,LINE))
 C         Don't calculate at wavenumbers more than MAXDV away
           IF(ABS(DV).LE.MAXDV)THEN
 
@@ -106,7 +111,18 @@ C              if H2O continuum is turned on
      3       SLIN(IB,LINE),ELIN(IB,LINE),ALIN(IB,LINE),SBLIN(IB,LINE),
      4       TDW(IB,LINE),TDWS(IB,LINE),LLQ(IB,LINE),DOUBV(IB,LINE),
      5       FNH3,FH2)
-
+C              IF(LINE.EQ.1)THEN
+C               PRINT*,IPROC(IGAS),IDGAS(IGAS),VV
+C               PRINT*,TCORDW(LAYER,IGAS),TCORS1(LAYER,IGAS),
+C     1  TCORS2(LAYER)
+C               PRINT*,PRESS(LAYER),TEMP(LAYER),FRAC(LAYER,IGAS),
+C     1  VLIN(IB,LINE)
+C               PRINT*,SLIN(IB,LINE),ELIN(IB,LINE),ALIN(IB,LINE),
+C     1  SBLIN(IB,LINE)
+C               PRINT*,TDW(IB,LINE),TDWS(IB,LINE),LLQ(IB,LINE),
+C     1  DOUBV(IB,LINE)
+C               PRINT*,FNH3,FH2
+C              ENDIF
              ENDIF
 
             CONVALS(K,LAYER,J)=CONVALS(K,LAYER,J)+CONVAL
@@ -122,6 +138,32 @@ C              if H2O continuum is turned on
 13    CONTINUE
 
 
+C     Extra continuum for linedata files that are so large that they
+C     have been stripped of weaker lines.
+      IF(IJLCO.GT.0.AND.IX.EQ.1)THEN
+       DO 299 IGAS=1,NGAS
+        IF(IDGAS(IGAS).EQ.IDLCO.AND.ISOGAS(IGAS).EQ.ISOLCO)THEN
+         print*,'Adding LCO for gas : ',IGAS,IDLCO,ISOLCO
+         FNH3=-1.0
+         FH2=-1.0
+         DO 300 LAYER=1,NLAYER
+          CALL CALC_LCO(VBIN,NBIN,WING,MAXDV,IPROC(IGAS),
+     1     IDGAS(IGAS),TCORDW(LAYER,IGAS),TCORS1(LAYER,IGAS),
+     2     TCORS2(LAYER),PRESS(LAYER),TEMP(LAYER),FRAC(LAYER,IGAS),
+     3     FNH3,FH2,VOUT,YOUT)
+          DO 301 I=1,NBIN
+           DO 302 K=1,IORDP1
+            VV=VBIN(I)+CONWAV(K)
+            CALL VERINT(VOUT,YOUT,NBIN+1,CONVAL,SNGL(VV))
+            print*,K,LAYER,I,CONVALS(K,LAYER,I),CONVAL
+            CONVALS(K,LAYER,I)=CONVALS(K,LAYER,I)+CONVAL
+302        CONTINUE
+301       CONTINUE
+300      CONTINUE
+        ENDIF
+299    CONTINUE
+      ENDIF
+
 C     Convert continuum values to polynomial coefficients
       DO 200 LAYER=1,NLAYER
        DO 202 J=1,NBIN
@@ -134,6 +176,8 @@ C     Convert continuum values to polynomial coefficients
 205     CONTINUE
 202    CONTINUE
 200   CONTINUE
+
+
 
       RETURN
 

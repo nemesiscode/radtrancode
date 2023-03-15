afbd255c1f45a2e9debee55fdff0201cebec8e67
nemesiscode <patrick.irwin@physics.ox.ac.uk>
2023-03-15 10:07:18 +0000

    Updated treatment of net fluxes in streamflux.f

--- radtran/cirsrad/streamflux.f
+++ radtran/cirsrad/streamflux.f
@@ -1,5 +1,5 @@
-      subroutine streamflux(nlays,nmu,mu1,wt1,radg,galb,ig,umif,uplf,
-     1 fup,fdown,ftop)
+      subroutine streamflux(nlays,nmu,mu1,wt1,radg,lowbc,galb,ig,
+     1 umif,uplf,fup,fdown,ftop)
 C     ***************************************************************
 C     Subroutine to format output of scloud11flux into a more usable
 C     internal radiation field and output to a meta file.
@@ -10,6 +10,7 @@ C	nmu	integer	Number of zenith quadrature angles
 C	mu1(maxmu) double precision	zenith angle values
 C	wt1(maxmu) double precision	zenith angle weights
 C	radg(maxmu)	real	Ground radiance field
+C	lowbc	integer	Set to 1 if Extra lambertian layer added
 C       galb	double precision	Ground albedo	
 C	ig	integer	G-ordinate
 C	umif(maxmu,maxscatlay,maxf) real Radiance field going
@@ -26,20 +27,23 @@ C       ftop(maxg)	real	Upward flux at top of top layer (i.e. to space)
 C
 C     Documented by Pat Irwin	13/3/14
 C     Converted from dumpflux by Pat Irwin 13/3/14
+C     Major overhaul and simplification by Pat Irwin 15/3/23
 C
 C     ***************************************************************
       implicit none
       INCLUDE '../includes/arrdef.f'
       INCLUDE '../includes/constdef.f'
-      integer nlays,nmu,i,ilay,jlay,ig
+      integer nlays,nmu,i,ilay,jlay,ig,lowbc
       real x,umif(maxmu,maxscatlay,maxf),uplf(maxmu,maxscatlay,maxf)
       real radg(maxmu),fup(maxlay,maxg),fdown(maxlay,maxg)
+      real tup(maxlay,maxg),tdown(maxlay,maxg)
       real ftop(maxg),xfac,xnorm
       double precision mu1(maxmu),wt1(maxmu),galb
 
 
 C     Add in bottom radiance to complete radiance field at base of
-C     atmosphere. NB. Scattering flux defines uplf to be radiance going 
+C     atmosphere if no Lambertian ground layer included in calculation
+C     NB. Scattering flux defines uplf to be radiance going 
 C     downwards out of bottom of layer and umif to be the radiance going
 C     upwards out of the top. We change the meaning here to be the 
 C     total radiance field (up and down) at the bottom of each layer.  
@@ -53,82 +57,50 @@ C     First integrate fluxes over zenith solid angle
 
 
 C     Find correction for any quadrature errors
-      xfac=0.
-      do i=1,nmu
-       xfac=xfac+sngl(mu1(i)*wt1(i))
-      enddo
-C      print*,'xfac = ',xfac
-      xnorm=PI/xfac
+C      xfac=0.
+C      do i=1,nmu
+C       xfac=xfac+sngl(mu1(i)*wt1(i))
+C      enddo
+C      print*,'xfac (should be 0.5) = ',xfac
+C      xnorm=PI/xfac
       xnorm=2*PI
 
-C      print*,'Flux down out of bottom, flux up out of top'
-C      print*,'Layer, Fdown, Fup'
-C      do ilay=1,nlays+1
-C       fup(ilay,ig)=0.
-C       fdown(ilay,ig)=0.
-C       do i=1,nmu
-C        fdown(ilay,ig)=fdown(ilay,ig)+
-C     &          sngl(mu1(i)*wt1(i))*uplf(i,ilay,1)
-C        fup(ilay,ig)=fup(ilay,ig)+
-C     &          sngl(mu1(i)*wt1(i))*umif(i,ilay,1)
-C       enddo
-C       print*,ilay,xnorm*fdown(ilay,ig),xnorm*fup(ilay,ig)
-C      enddo
-
+C     First use scattering convention of height order where we start at top and go
+C     down.
       do 10 ilay=1,nlays
-
-C      reverse order of output so that 1 is for the bottom layer
-       jlay=nlays+1-ilay
-
-       if(ilay.eq.1)then
-C       Bottom layer
-        fdown(ilay,ig)=0.
+        tdown(ilay,ig)=0.
+        tup(ilay,ig)=0.
         do i=1,nmu
-         fdown(ilay,ig)=fdown(ilay,ig)+
-     &		sngl(mu1(i)*wt1(i))*uplf(i,jlay,1)
-        enddo
+         tdown(ilay,ig)=tdown(ilay,ig)+
+     &		xnorm*sngl(mu1(i)*wt1(i))*uplf(i,ilay,1)
 
-        fup(ilay,ig)=0.
+         tup(ilay,ig)=tup(ilay,ig)+sngl(mu1(i)*wt1(i))*
+     &		xnorm*umif(i,ilay+1,1)
+        enddo
+10    continue 
+
+C     If no extra layer added, assume upwards radiation at bottom of bottom layer is just the 
+C     ground radiation.
+      if(lowbc.eq.0)then
+        tup(nlays,ig)=0.
         do i=1,nmu
-C        Thermal emission from ground
-        fup(ilay,ig)=fup(ilay,ig)+sngl(mu1(i)*wt1(i))*radg(i)*
-     &		(1.0-sngl(galb))
-C	 Reflected radiance from ground
-        fup(ilay,ig)=fup(ilay,ig)+sngl(mu1(i)*wt1(i))*
-     &        uplf(i,jlay,1)*sngl(galb)
+         tup(nlays,ig)=tup(nlays,ig)+xnorm*sngl(mu1(i)*wt1(i))*radg(i)
         enddo
+      endif
 
-       else
-
-        fdown(ilay,ig)=0
-        fup(ilay,ig)=0
-
-        do i=1,nmu
-         fdown(ilay,ig)=fdown(ilay,ig)+sngl(mu1(i)*wt1(i))*
-     &		uplf(i,jlay,1)
-C         fup(ilay,ig)=fup(ilay,ig)+sngl(mu1(i)*wt1(i))*
-C     &		umif(i,jlay,1)
-         fup(ilay,ig)=fup(ilay,ig)+sngl(mu1(i)*wt1(i))*
-     &		umif(i,jlay+1,1)
-        enddo
-C        print*,ilay,xnorm*fdown(ilay,ig),xnorm*fup(ilay,ig)
-       endif
+C     Set top upwards radiation to upwards flux from top layer
        
-       if(ilay.eq.nlays)then
+      ftop(ig)=0.
+      do i=1,nmu
+        ftop(ig)=ftop(ig)+xnorm*sngl(mu1(i)*wt1(i))*umif(i,1,1)
+      enddo
 
-        ftop(ig)=0.
-        do i=1,nmu
-         ftop(ig)=ftop(ig)+xnorm*sngl(mu1(i)*wt1(i))*umif(i,jlay,1)
-        enddo
-
-       endif
-
-       fdown(ilay,ig)=fdown(ilay,ig)*xnorm
-       fup(ilay,ig)=fup(ilay,ig)*xnorm
-
-10    continue
-
-    
+C     Reverse order of final output arrays
+      do 15 ilay=1,nlays
+       jlay=nlays+1-ilay
+       fdown(jlay,ig)=tdown(ilay,ig)
+       fup(jlay,ig)=tup(ilay,ig)
+15    continue
 
       return
 
